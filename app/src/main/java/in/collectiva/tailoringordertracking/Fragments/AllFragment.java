03be11c6b326a2d69b7df95f53989a0fcfa49dc3
dhivya <dhivya@shss.co.in>
2017-07-18 12:42:11 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/in/collectiva/tailoringordertracking/Fragments/AllFragment.java
    #	app/src/main/res/layout/activity_order_items.xml

--- app/src/main/java/in/collectiva/tailoringordertracking/Fragments/AllFragment.java
+++ app/src/main/java/in/collectiva/tailoringordertracking/Fragments/AllFragment.java
@@ -1,11 +1,9 @@
 package in.collectiva.tailoringordertracking.Fragments;
 
-<<<<<<< HEAD
+
 import android.content.DialogInterface;
-=======
 import android.app.Activity;
 import android.app.DatePickerDialog;
->>>>>>> origin/master
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,13 +12,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-<<<<<<< HEAD
-=======
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
->>>>>>> origin/master
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
@@ -47,16 +42,14 @@ import in.collectiva.tailoringordertracking.cConstant.clsParameters;
 //Our class extending fragment
 public class AllFragment extends Fragment {
 
-<<<<<<< HEAD
+
     private String jsonString, lSelectedOrderId;
-=======
+
     private TextView ltxtAllNoRecords;
     private ListView lstAll;
     private Spinner spinner;
     private EditText edAsOnDate;
 
-    private String jsonString;
->>>>>>> origin/master
     private static final String NAMESPACE = "http://ws.collectiva.in/";
     private static final String REQURL = "http://ws.collectiva.in/AndroidTailoringService.svc"; //"http://ws.collectiva.in/AndroidTestService.svc";
     final String SOAP_ACTION = "http://ws.collectiva.in/IAndroidTailoringService/"; //http://ws.collectiva.in/IAndroidTestService/RegisterUser";
@@ -96,106 +89,6 @@ public class AllFragment extends Fragment {
         // get user data from session
         HashMap<String, String> user = session.getUserDetails();
 
-        // User Id
-<<<<<<< HEAD
-        String lUserId = user.get(SessionManagement.KEY_USERID);
-
-        /*ArrayList<clsParameters> lstParameters = new ArrayList<>();
-        clsParameters objParam = new clsParameters();
-        objParam.ParameterName = "UserId";
-        objParam.ParameterValue = lUserId;
-        lstParameters.add(objParam);
-
-        objParam = new clsParameters();
-        objParam.ParameterName = "OrderId";
-        objParam.ParameterValue = "0";
-        lstParameters.add(objParam);
-
-        String lMethodName = "GetOrders";
-        jsonString = objCRUD.GetScalar(NAMESPACE, lMethodName, REQURL, SOAP_ACTION + lMethodName, lstParameters);*/
-
-        ArrayList<clsParameters> lstParameters = new ArrayList<>();
-        clsParameters objParam = new clsParameters();
-        objParam.ParameterName = "UserId";
-        objParam.ParameterValue = lUserId;
-        lstParameters.add(objParam);
-
-        objParam = new clsParameters();
-        objParam.ParameterName = "StatusId";
-        objParam.ParameterValue = "1";
-        lstParameters.add(objParam);
-
-        String lMethodName = "GetOrdersByStatus";
-        jsonString = objCRUD.GetScalar(NAMESPACE, lMethodName, REQURL, SOAP_ACTION + lMethodName, lstParameters);
-
-        TextView ltxtAllNoRecords = (TextView) view.findViewById(R.id.txtAllNoRecords);
-        ltxtAllNoRecords.setText("");
-
-        if (jsonString.equals("0")) {
-            ltxtAllNoRecords.setText("No records found!");
-        } else {
-
-            ListView lstAll = (ListView) view.findViewById(R.id.lstAllOrderList);
-            SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), JSONOrder.newInstance().GetJSONOrderList(jsonString),
-                    R.layout.orderrow, new String[]{"OrderId", "OrderDetail", "DeliveryDate", "Status"},
-                    new int[]{R.id.txtOrderRowId, R.id.txtOrderRowDetail, R.id.txtOrderRowDeliveryDate,
-                            R.id.txtOrderRowStatus});
-
-            lstAll.setAdapter(simpleAdapter);
-
-            lstAll.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    // selected item
-                    lSelectedOrderId = ((TextView) view.findViewById(R.id.txtOrderRowId)).getText().toString();
-                    String OrderDetail = ((TextView) view.findViewById(R.id.txtOrderRowDeliveryDate)).getText().toString();
-
-                    // 1. Instantiate an AlertDialog.Builder with its constructor
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-                    // 2. Chain together various setter methods to set the dialog characteristics
-                    builder.setMessage("Are you sure to Update the Status as 'In-Progress' for the Order " + OrderDetail + "?")
-                            .setTitle("");
-
-                    // Add the buttons
-                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User clicked OK button
-
-                            ArrayList<clsParameters> lstParameters = new ArrayList<>();
-                            clsParameters objParam = new clsParameters();
-                            objParam.ParameterName = "OrderID";
-                            objParam.ParameterValue = lSelectedOrderId;
-                            lstParameters.add(objParam);
-
-                            objParam = new clsParameters();
-                            objParam.ParameterName = "StatusID";
-                            objParam.ParameterValue = "2";
-                            lstParameters.add(objParam);
-
-                            String lMethodName = "UpdateOrderStatus";
-                            jsonString = objCRUD.GetScalar(NAMESPACE, lMethodName, REQURL, SOAP_ACTION + lMethodName, lstParameters);
-
-                            //Refresh the Grid in the Parent
-                            MyOrders activity = (MyOrders) getActivity();
-                            activity.BindTab(0);
-                        }
-                    });
-                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                        }
-                    });
-
-                    // 3. Get the AlertDialog from create()
-                    AlertDialog dialog = builder.create();
-
-
-                    dialog.show();
-                }
-            });
-        }
-=======
         lUserId = user.get(SessionManagement.KEY_USERID);
 
         edAsOnDate = (EditText) view.findViewById(R.id.edtAsonDate);
@@ -247,7 +140,6 @@ public class AllFragment extends Fragment {
 
         txtDate = (EditText) view.findViewById(R.id.edtAsonDate);
         txtDate.setOnClickListener(lbtnDatePickerListener);
->>>>>>> origin/master
     }
 
     private View.OnClickListener lbtnGetOrderDetail = new View.OnClickListener() {
