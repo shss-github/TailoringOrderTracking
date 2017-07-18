755357e5e40172195f6d5978903fce0a66ea4457
Gopi <gopi@shss.co.in>
2017-07-18 12:17:05 +0530

    User Detail Update Activtiy

--- app/src/main/java/in/collectiva/tailoringordertracking/Fragments/AllFragment.java
+++ app/src/main/java/in/collectiva/tailoringordertracking/Fragments/AllFragment.java
@@ -1,16 +1,26 @@
 package in.collectiva.tailoringordertracking.Fragments;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
@@ -28,6 +38,11 @@ import in.collectiva.tailoringordertracking.cConstant.clsParameters;
 //Our class extending fragment
 public class AllFragment extends Fragment {
 
+    private TextView ltxtAllNoRecords;
+    private ListView lstAll;
+    private Spinner spinner;
+    private EditText edAsOnDate;
+
     private String jsonString;
     private static final String NAMESPACE = "http://ws.collectiva.in/";
     private static final String REQURL = "http://ws.collectiva.in/AndroidTailoringService.svc"; //"http://ws.collectiva.in/AndroidTestService.svc";
@@ -38,7 +53,10 @@ public class AllFragment extends Fragment {
 
     // Session Manager Class
     SessionManagement session;
+    String lUserId = "";
 
+    EditText txtDate;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     //Overriden method onCreateView
     @Override
@@ -66,38 +84,130 @@ public class AllFragment extends Fragment {
         HashMap<String, String> user = session.getUserDetails();
 
         // User Id
-        String lUserId = user.get(SessionManagement.KEY_USERID);
+        lUserId = user.get(SessionManagement.KEY_USERID);
 
-        ArrayList<clsParameters> lstParameters = new ArrayList<>();
-        clsParameters objParam = new clsParameters();
-        objParam.ParameterName = "UserId";
-        objParam.ParameterValue = lUserId;
-        lstParameters.add(objParam);
+        edAsOnDate = (EditText) view.findViewById(R.id.edtAsonDate);
+        edAsOnDate.setOnClickListener(lbtnDatePickerListener);
+        edAsOnDate.setVisibility(View.GONE);
 
-        objParam = new clsParameters();
-        objParam.ParameterName = "OrderId";
-        objParam.ParameterValue = "0";
-        lstParameters.add(objParam);
+        spinner = (Spinner) view.findViewById(R.id.cboOption);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (parent.getItemAtPosition(position).toString()) {
+                    case "All":
+                        edAsOnDate.setVisibility(View.GONE);
+                        edAsOnDate.setText("");
+                        break;
+                    case "Today":
+                        edAsOnDate.setVisibility(View.GONE);
+                        edAsOnDate.setText("");
+                        break;
+                    case "Next 7 days":
+                        edAsOnDate.setVisibility(View.GONE);
+                        edAsOnDate.setText("");
+                        break;
+                    case "As on":
+                        edAsOnDate.setVisibility(View.VISIBLE);
+                        edAsOnDate.setText("");
+                        break;
+                }
+            }
 
-        String lMethodName = "GetOrders";
-        jsonString = objCRUD.GetScalar(NAMESPACE, lMethodName, REQURL, SOAP_ACTION + lMethodName, lstParameters);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-        TextView ltxtAllNoRecords = (TextView) view.findViewById(R.id.txtAllNoRecords);
-        ltxtAllNoRecords.setText("");
+            }
+        });
 
-        if (jsonString.equals("0")) {
-            ltxtAllNoRecords.setText("No records found!");
-        } else {
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.orders_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
-            ListView lstAll = (ListView) view.findViewById(R.id.lstAllOrderList);
-            SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), JSONOrder.newInstance().GetJSONOrderList(jsonString),
-                    R.layout.orderrow, new String[]{"OrderId", "OrderDetail", "DeliveryDate", "Status"},
-                    new int[]{R.id.txtOrderRowId, R.id.txtOrderRowDetail, R.id.txtOrderRowDeliveryDate,
-                            R.id.txtOrderRowStatus});
+        ltxtAllNoRecords = (TextView) view.findViewById(R.id.txtAllNoRecords);
+        lstAll = (ListView) view.findViewById(R.id.lstAllOrderList);
 
-            lstAll.setAdapter(simpleAdapter);
-        }
+        Button btnGetResult = (Button) view.findViewById(R.id.btnGetOrder);
+        btnGetResult.setOnClickListener(lbtnGetOrderDetail);
+
+        txtDate = (EditText) view.findViewById(R.id.edtAsonDate);
+        txtDate.setOnClickListener(lbtnDatePickerListener);
     }
 
+    private View.OnClickListener lbtnGetOrderDetail = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            boolean ProceedToGet = true;
+            if (spinner.getSelectedItem().toString().trim().equals("As on")) {
+                if (edAsOnDate.getText().toString().trim().equals("")) {
+                    ProceedToGet = false;
+                    edAsOnDate.setError("Date is required!");
+                }
+            }
+
+            if (ProceedToGet) {
+                try {
+                    ArrayList<clsParameters> lstParameters = new ArrayList<>();
+                    clsParameters objParam = new clsParameters();
+                    objParam.ParameterName = "UserId";
+                    objParam.ParameterValue = lUserId;
+                    lstParameters.add(objParam);
+
+                    objParam = new clsParameters();
+                    objParam.ParameterName = "FilterBy";
+                    objParam.ParameterValue = spinner.getSelectedItem().toString();
+                    lstParameters.add(objParam);
+
+                    objParam = new clsParameters();
+                    objParam.ParameterName = "FilterDate";
+                    objParam.ParameterValue = edAsOnDate.getText().toString();
+                    lstParameters.add(objParam);
+
+                    String lMethodName = "GetOrdersByParticular";
+                    jsonString = objCRUD.GetScalar(NAMESPACE, lMethodName, REQURL, SOAP_ACTION + lMethodName, lstParameters);
+
+                   ltxtAllNoRecords.setText("");
+
+                    if (jsonString.equals("0")) {
+                        ltxtAllNoRecords.setText("No records found!");
+                    } else {
+                       // ltxtAllNoRecords.setText("records found!");
+                        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), JSONOrder.newInstance().GetJSONOrderList(jsonString),
+                                R.layout.orderrow, new String[]{"OrderId", "OrderDetail", "DeliveryDate", "Status"},
+                                new int[]{R.id.txtOrderRowId, R.id.txtOrderRowDetail, R.id.txtOrderRowDeliveryDate,
+                                        R.id.txtOrderRowStatus});
+
+                        lstAll.setAdapter(simpleAdapter);
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    };
+
+    private View.OnClickListener lbtnDatePickerListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+    };
 }
 
